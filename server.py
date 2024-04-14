238c7fbc9e2b81e542db737faca3417132801fa5
Кирилл <kirovch28@gmail.com>
2024-04-14 20:09:42 +0900

    test_commit(2)

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 import os
 import random
 import sqlite3
+import requests
 from flask import Flask
-from flask import request, render_template, redirect, url_for, send_from_directory, send_file, Response
+from flask import request, render_template, redirect, url_for, send_from_directory, send_file, Response, abort, \
+    make_response
 import json
 import socket
 import shutil
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 
 @app.route('/main')
@@ -17,6 +20,7 @@ def main():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    print(request.cookies.get('user_id'))
     if request.method == 'GET':
         return render_template('login.html')
     elif request.method == 'POST':
@@ -47,8 +51,12 @@ def login():
                 with open(f'static/user/{request.form.get("name").strip()}/identification.json', 'r',
                           encoding='utf8') as file:
                     fileData = json.load(file)
-                return render_template('login_continue.html', name=request.form.get('name').strip(), id=fileData['id'],
-                                       number=fileData['number'], portal=fileData['portal'])
+                response = make_response(
+                    render_template('login_continue.html', name=request.form.get('name').strip(), id=fileData['id'],
+                                    number=fileData['number'], portal=fileData['portal']))
+                response.set_cookie('user_id',
+                                    f'{random.randint(1290, 278327873298)}')
+                return response
             else:
                 cursor.close()
                 return render_template('login.html', goodReg='ВТН', email=request.form.get('email'),
@@ -61,21 +69,10 @@ def login_continue(name):
         with open(f'static/user/{name.strip()}/identification.json', 'r',
                   encoding='utf8') as file:
             fileData = json.load(file)
-            fileData['ip'] = f'{str(request.data)[9:-3]}'
+            fileData['ip'] = f'{str(request.headers.get("X-Forwarded-For"))}'
         with open(f'static/user/{name.strip()}/identification.json', 'w',
                   encoding='utf8') as file:
             json.dump(fileData, file)
-
-
-@app.route('/login_check/<name>', methods=['GET', 'POST'])
-def login_check(name):
-    if request.data:
-        with open(f'static/user/{name.strip()}/identification.json', 'r',
-                  encoding='utf8') as file:
-            fileData = json.load(file)
-        if fileData['ip'] == f'{str(request.data)[9:-3]}':
-            with open(f'static/user/{name.strip()}/сheck.txt', 'w', encoding='utf8') as file:
-                file.write('done')
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -113,12 +110,11 @@ def register():
                 iden_file = open("identification.json", "w")
                 iden_file.close()
                 os.replace("identification.json", f"static/user/{request.form.get('name').strip()}/identification.json")
-                check_file = open('сheck.txt', 'w')
-                check_file.close()
-                os.replace("сheck.txt", f"static/user/{request.form.get('name').strip()}/сheck.txt")
                 os.mkdir(f'static/user/{request.form.get("name").strip()}/avatar')
                 os.mkdir(f'static/user/{request.form.get("name").strip()}/photos')
                 os.mkdir(f'static/user/{request.form.get("name").strip()}/video')
+                os.mkdir(f'static/user/{request.form.get("name").strip()}/entrys')
+                os.mkdir(f'static/user/{request.form.get("name").strip()}/entrys/files')
                 shutil.copy('avatar.png', f'static/user/{request.form.get("name").strip()}/avatar/avatar.png')
                 shutil.copy('profile.json', f'static/user/{request.form.get("name").strip()}/profile.json')
                 return render_template('main.html')
@@ -181,6 +177,33 @@ def addget_video(video_name):
     )
 
 
+@app.route('/addenvideo/<video_name>/<name>')
+def addenget_video(video_name, name):
+    video_path = f'static/user/{name}/entrys/files/{video_name}'
+    file_size = os.path.getsize(video_path)
+    headers = {}
+
+    if 'Range' in request.headers:
+        range_header = request.headers.get('Range')
+        start = 0
+        end = file_size - 1
+        chunk_size = file_size
+        status_code = 200
+        headers['Content-Range'] = f"bytes {start}-{end}/{file_size}"
+        headers['Content-Length'] = chunk_size
+    else:
+        chunk_size = file_size
+
+    headers['Accept-Ranges'] = 'bytes'
+
+    return Response(
+        open(video_path, mode='rb').read(chunk_size),
+        status=status_code,
+        headers=headers,
+        content_type='video/mp4'
+    )
+
+
 @app.route('/acceptPhoto/<name>/<id>/<number>/<portal>/<filename>', methods=['GET', 'POST'])
 def acceptPhoto(name, id, number, portal, filename):
     if request.method == 'GET':
@@ -193,13 +216,12 @@ def acceptPhoto(name, id, number, portal, filename):
 @app.route('/addphoto/<name>/<id>/<number>/<portal>', methods=['GET', 'POST'])
 def addphoto(name, id, number, portal):
     if request.method == 'GET':
-        with open(f'static/user/{name}/сheck.txt', 'r', encoding='utf8') as file:
-            if not file.readline():
-                return render_template('profileiden.html', name=name, id=id,
-                                       number=number, portal=portal, place='addphoto')
+        with open(f'static/user/{name.strip()}/identification.json', 'r',
+                  encoding='utf8') as file:
+            fileData = json.load(file)
+            if fileData['ip'] != f'{str(request.headers.get("X-Forwarded-For"))}':
+                return abort(403)
             else:
-                f = open(f'static/user/{name}/сheck.txt', 'w')
-                f.close()
                 return render_template('addphoto.html')
     elif request.method == 'POST':
         if request.files.get('file'):
@@ -220,13 +242,12 @@ def acceptVideo(name, id, number, portal, filename):
 @app.route('/addvideo/<name>/<id>/<number>/<portal>', methods=['GET', 'POST'])
 def addvideo(name, id, number, portal):
     if request.method == 'GET':
-        with open(f'static/user/{name}/сheck.txt', 'r', encoding='utf8') as file:
-            if not file.readline():
-                return render_template('profileiden.html', name=name, id=id,
-                                       number=number, portal=portal, place='addvideo')
+        with open(f'static/user/{name.strip()}/identification.json', 'r',
+                  encoding='utf8') as file:
+            fileData = json.load(file)
+            if fileData['ip'] != f'{str(request.headers.get("X-Forwarded-For"))}':
+                return abort(403)
             else:
-                f = open(f'static/user/{name}/сheck.txt', 'w')
-                f.close()
                 return render_template('addvideo.html')
     elif request.method == 'POST':
         if request.files.get('file'):
@@ -235,16 +256,45 @@ def addvideo(name, id, number, portal):
             return redirect(f'/acceptVideo/{name}/{id}/{number}/{portal}/({name}){f.filename}')
 
 
+@app.route('/makeanentry/<name>/<id>/<number>/<portal>', methods=['GET', 'POST'])
+def makeanentry(name, id, number, portal):
+    if request.method == 'GET':
+        with open(f'static/user/{name.strip()}/identification.json', 'r',
+                  encoding='utf8') as file:
+            fileData = json.load(file)
+            if fileData['ip'] != f'{str(request.headers.get("X-Forwarded-For"))}':
+                return abort(403)
+            else:
+                return render_template('makeanentry.html')
+    elif request.method == 'POST':
+        files = []
+        for key in request.files.keys():
+            f = request.files[key]
+            f.save(
+                f'static/user/{name}/entrys/files/{len(os.listdir(f"static/user/{name}/entrys/files")) + 1}file{f.filename[-4:]}')
+            files.append(
+                str(len(os.listdir(f"static/user/{name}/entrys/files"))) + f'file{request.files[key].filename[-4:]}')
+        data = {'topic': f'{request.form.get("topic")}',
+                'text': f'{request.form.get("text")}',
+                'url': f'{request.form.get("url")}'}
+        if request.files:
+            data['files'] = files
+        with open(f'static/user/{name}/entrys/entry{len(os.listdir(f"static/user/{name}/entrys"))}.json', 'w',
+                  encoding='utf8') as file:
+            json.dump(data, file)
+        return redirect(f'/profile/{name}/{id}/{number}/{portal}')
+
+
 @app.route('/profile/<name>/<id>/<number>/<portal>', methods=['GET', 'POST'])
 def profile(name, id, number, portal):
     if request.method == 'GET':
-        with open(f'static/user/{name}/сheck.txt', 'r', encoding='utf8') as file:
-            if not file.readline():
-                return render_template('profileiden.html', name=name, id=id,
-                                       number=number, portal=portal, place='profile')
+        print(request.cookies.get('user_id'))
+        with open(f'static/user/{name.strip()}/identification.json', 'r',
+                  encoding='utf8') as file:
+            fileData = json.load(file)
+            if fileData['ip'] != f'{str(request.headers.get("X-Forwarded-For"))}':
+                return abort(403)
             else:
-                f = open(f'static/user/{name}/сheck.txt', 'w')
-                f.close()
                 with open(f'static/user/{name}/identification.json', 'r', encoding='utf8') as file:
                     fileData = json.load(file)
                 path = f'/static/user/{name}/avatar/{os.listdir(f"static/user/{name}/avatar")[0]}'
@@ -256,10 +306,17 @@ def profile(name, id, number, portal):
                     videos.append(video)
                 with open(f'static/user/{name}/profile.json', 'r', encoding='utf8') as file:
                     fileDataprofile = json.load(file)
+                endata = []
+                if len(os.listdir(f'static/user/{name}/entrys')) > 1:
+                    for num in range(1, len(os.listdir(f'static/user/{name}/entrys'))):
+                        with open(f'static/user/{name}/entrys/entry{num}.json', 'r', encoding='utf8') as file:
+                            fileDataen = json.load(file)
+                            endata.append(fileDataen)
+                print(endata)
                 if fileData['id'] == id and fileData['number'] == number and fileData['portal'] == portal:
                     return render_template('profile.html', name=name, path_photos=photos, subs=fileDataprofile['Subs'],
                                            id=id,
-                                           number=number, portal=portal, path=path, path_videos=videos)
+                                           number=number, portal=portal, path=path, path_videos=videos, endata=endata)
 
 
 if __name__ == '__main__':
